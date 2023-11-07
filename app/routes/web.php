ec3ff00a7d1948bb5ad503ade778174661aa0b1e
Wafa-Ag <prsscontact@gmail.com>
2023-11-07 22:38:31 +0100

    ajouter les nouvelles routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BouteilleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('liste-produits/{page}', [BouteilleController::class, 'getProduits'])->name('listeProduits');

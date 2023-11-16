4e188b2dd7df5028ecc7a5590f64f8aa254a6d6f
MarieLoup46 <melaniebel41@gmail.com>
2023-11-16 10:53:33 -0500

    Uniformisation des classes dans auth.css

--- app/resources/views/auth/admin-user-list.blade.php
+++ app/resources/views/auth/admin-user-list.blade.php
@@ -3,43 +3,43 @@
 @section('content')
     <div>
         <div class="auth__header">
-            <h1 class="auth_header_h1-title">Liste des usagers</h1>
+            <h1 class="auth__header_h1-title">Liste des usagers</h1>
         </div>
 
         <!-- Message affiché lorsque un utilisateur est supprimé -->
         @if(session('success'))
-            <div class="auth_u-list_success_delete">
+            <div class="auth__u-list_success_delete">
                 {{session('success')}}
             </div>
         @endif
 
-        <div class="auth_user-list">
+        <div class="auth__user-list">
             <table>
                 <thead>
                     <tr>
-                        <th class="auth_user-list_th">Id</th>
-                        <th class="auth_user-list_th">Nom</th>
-                        <th class="auth_user-list_th">Prénom</th>
-                        <th class="auth_user-list_th"></th>
+                        <th class="auth__user-list_th">Id</th>
+                        <th class="auth__user-list_th">Nom</th>
+                        <th class="auth__user-list_th">Prénom</th>
+                        <th class="auth__user-list_th"></th>
                     </tr>
                 </thead>
                 <tbody>
                     <!-- Affiche les utilisateurs -->
                     @forelse($users as $user)
                         <tr>
-                            <td class="auth_user-list_td">{{ $user->id }}</td>
-                            <td class="auth_user-list_td">{{ $user->nom }}</td>
-                            <td class="auth_user-list_td">{{ $user->prenom }}</td>
-                            <td class="auth_user-list_td">
+                            <td class="auth__user-list_td">{{ $user->id }}</td>
+                            <td class="auth__user-list_td">{{ $user->nom }}</td>
+                            <td class="auth__user-list_td">{{ $user->prenom }}</td>
+                            <td class="auth__user-list_td">
                             <form action="{{ route('user.delete', $user->id) }}" method="post">
                                 @csrf
                                 @method('delete')
-                                <input class="auth_user-list_btn" type="submit" value="SUPPRIMER">
+                                <input class="auth__user-list_btn" type="submit" value="SUPPRIMER">
                             </form>
                             </td>
                         </tr>
                     @empty
-                        <td class="auth_user-list_td" colspan="4">Aucun usagé disponible</td>
+                        <td class="auth__user-list_td" colspan="4">Aucun usagé disponible</td>
                     @endforelse
                 </tbody>
             </table>
--- app/resources/views/auth/show.blade.php
+++ app/resources/views/auth/show.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <div>
         <div>
-            <h1 class="auth_header_h1-title">Profil</h1>
+            <h1 class="auth__header_h1-title">Profil</h1>
         </div>
 
         <div>
-            <h2 class="auth_h2-title">INFORMATIONS</h2>
+            <h2 class="auth__h2-title">INFORMATIONS</h2>
         </div>
 
-        <form class="auth_form_profil">
+        <form class="auth__form_profil">
             <label for="nom" id="nom">NOM</label>
             <input type="text" id="nom" name="nom" value="{{ $user->nom }}">
 
@@ -20,14 +20,14 @@
             <label for="email" id="email">COURRIEL</label>
             <input type="text" id="email" name="email" value="{{ $user->email }}">
 
-            <button class="auth_profil_btn">MODIFIER MES INFORMATIONS</button>
+            <button class="auth__profil_btn">MODIFIER MES INFORMATIONS</button>
 
 
-            <h2 class="auth_h2-title auth_profil_compte">COMPTE</h2>
+            <h2 class="auth__h2-title auth_profil_compte">COMPTE</h2>
 
-            <button class="auth_profil_btn">ME DÉCONNECTER</button>
+            <button class="auth__profil_btn">ME DÉCONNECTER</button>
 
-            <button class="auth_profil_btn auth_profil_delete_btn">SUPPRIMER MON COMPTE</button>
+            <button class="auth__profil_btn auth_profil_delete_btn">SUPPRIMER MON COMPTE</button>
         </form>
     </div>
 @endsection

d2f06f231293ed14d45d9293484b5d7f5be1da99
vanessa <andrade9290@gmail.com>
2023-11-17 01:31:33 -0500

    change color

--- app/resources/views/cellier/index.blade.php
+++ app/resources/views/cellier/index.blade.php
@@ -1,33 +1,40 @@
 @extends('layouts.app')
-@section('title', 'Celliers')
-@section('content')
-    <div class="cellier-row">
-        <div class="cellier-main-content">
-            <div class="cellier-title-section">
-                <h3 class="cellier-title">Mes celliers</h3>
-                <div class="cellier-button-section">
-                    <a href="{{ route('cellier.create') }}" class="cellier-add-button">AJOUTER UN CELLIER</a>
-                </div>
-            </div>
 
-            <div class="cellier-list">
-                @foreach($items as $index => $item)
-                    <div class="cellier-item">
-                        <a class="full-link" href="{{ route('cellier.show', $item->id) }}"></a>
-                        <div class="cellier-item-image">
-                            <img alt="{{ $item->nom }}" src="{{ asset('icons/' . $random_icon) }}" class="cellier-icon" />
-                        </div>
-                        <div class="cellier-item-info">
-                            <span class="cellier-name">{{ $item->nom }}</span>
-                            <small class="cellier-bottle-count">{{ $index }} Bouteille</small>
-                        </div>
-                        <div class="cellier-item-action">
-                            <img src="{{ asset('icons/info.png') }}" alt="info" class="cellier-info-icon" />
-                        </div>
-                    </div>
-                @endforeach
-            </div>
+@section('title', 'Celliers')
+
+@section('content')
+<main class="cellier-index-container">
+    <header class="cellier-index-titre">
+        <h3>Vos celliers</h3>
+        <div class="cellier-index-btn">
+            <form action="{{ route('cellier.create') }}" method="GET">
+            <button type="submit" class="cellier-index-btn-ajouter">AJOUTER UN CELLIER</button>
         </div>
-    </div>
+    </header>
+    <section class="cellier-index-liste">
+        @foreach($items as $index => $item)
+        <article class="cellier-index-item">
+            <a href="{{ route('cellier.show', $item->id) }}" class="cellier-index-select">
+                <img src="{{ asset('icons/' . $random_icon) }}" alt="{{ $item->nom }}" class="cellier-index-img" />
+                <div class="cellier-index-info">
+                    <h2 class="cellier-index-nom">{{ $item->nom }}</h2>
+                    <p class="cellier-index-bouteille-count">{{ $index }} Bouteille</p>
+                </div>
+            </a>
+        </article>
+        @endforeach
+    </section>
+
+    <nav class="RENOMEIE">
+        <ul class="RENOMEIE">
+            <li class="RENOMEIE"><a href="#" class="RENOMEIE">←</a></li>
+            <li class="RENOMEIE"><a href="#" class="RENOMEIE">1</a></li>
+            <li class="RENOMEIE"><a href="#" class="RENOMEIE">2</a></li>
+            <li class="RENOMEIE"><a href="#" class="RENOMEIE">3</a></li>
+            <li class="RENOMEIE"><a href="#" class="RENOMEIE">→</a></li>
+        </ul>
+    </nav>
+</main>
 @endsection
+
 @include('layouts.footer')

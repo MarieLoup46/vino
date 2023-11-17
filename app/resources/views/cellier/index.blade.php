bf0ba8bae2b90b909114d54e8982c1bf2fc7758a
vanessa <andrade9290@gmail.com>
2023-11-17 18:19:45 -0500

    edite css

--- app/resources/views/cellier/index.blade.php
+++ app/resources/views/cellier/index.blade.php
@@ -14,16 +14,18 @@
     <section class="cellier-index-list">
         @foreach($items as $index => $item)
         <article class="cellier-index-item">
-            <a href="{{ route('cellier.show', $item->id) }}" class="cellier-index-select">
-                <div class="cellier-index-item-content">
-                    <div class="cellier-index-item-image">
-                        <img alt="{{ $item->nom }}" src="{{ asset('icons/' . $random_icon) }}" class="cellier-index-icon" />
+            <div>
+                <a href="{{ route('cellier.show', $item->id) }}" class="cellier-index-select">
+                    <div class="cellier-index-item-content">
+                        <div class="cellier-index-item-image">
+                            <img alt="{{ $item->nom }}" src="{{ asset('icons/' . $random_icon) }}" class="cellier-index-icon" />
+                        </div>
+                        <div class="cellier-index-name">{{ $item->nom }}</div>
                     </div>
-                    <div class="cellier-index-name">{{ $item->nom }}</div>
-                </div>
-            </a>
+                </a>
+            </div>
             <div class="cellier-index-bottle-count-container">
-                <a href="URL_FOR_OTHER_PAGE" class="cellier-index-bottle-count-link">
+                <a href="{{ route('bouteille.recherche') }}" class="cellier-index-bottle-count-link">
                     <small class="cellier-index-bottle-count">{{ $index }} Bouteille</small>
                 </a>
             </div>
@@ -31,15 +33,26 @@
         @endforeach
     </section>
 
-    <nav class="RENOMEIE">
-        <ul class="RENOMEIE">
-            <li class="RENOMEIE"><a href="#" class="RENOMEIE">←</a></li>
-            <li class="RENOMEIE"><a href="#" class="RENOMEIE">1</a></li>
-            <li class="RENOMEIE"><a href="#" class="RENOMEIE">2</a></li>
-            <li class="RENOMEIE"><a href="#" class="RENOMEIE">3</a></li>
-            <li class="RENOMEIE"><a href="#" class="RENOMEIE">→</a></li>
-        </ul>
-    </nav>
+    <nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
+        <img src="{{ asset('path/to/your/previous-icon.png') }}" alt="Précédent">
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">
+        <img src="{{ asset('path/to/your/next-icon.png') }}" alt="Suivant">
+      </a>
+    </li>
+  </ul>
+</nav>
+
+
+
 </main>
 @endsection
 

7f558b490dc1637dc4fe4da185fbc5ccde551322
MarieLoup46 <melaniebel41@gmail.com>
2023-11-19 13:02:54 -0500

    Modif footer

--- app/resources/views/layouts/footer.blade.php
+++ app/resources/views/layouts/footer.blade.php
@@ -2,9 +2,7 @@
     <a href="/accueil"><img src="/icons/home.png" class="footer-icon" alt="accueil"/></a>
     <a href="/celliers"><img src="/icons/moncellier.png" class="footer-icon" alt="cellier"/></a>
     <a href="/recherche"><img src="/icons/rechercher.png" class="footer-icon" alt="recherche"/></a>
-    @if(auth()->guest())
-        <a href="/user"><img src="/icons/profil.png" class="footer-icon" alt="profil"/></a>
-    @else
+    @if(!auth()->guest())
         <a href="/user/{{ Auth::user()->id }}"><img src="/icons/profil.png" class="footer-icon" alt="profil"/></a>
     @endif
 </footer>

64b4753df2d81c245d2d8611ce147f46fab0ec14
MarieLoup46 <melaniebel41@gmail.com>
2023-11-21 14:17:57 -0500

    Menu affiche ok/problème affiche aussi en haut

--- app/resources/views/layouts/footer.blade.php
+++ app/resources/views/layouts/footer.blade.php
@@ -1,39 +1,20 @@
 <footer class="footer">
     <div class="footer-nav">
         <div class="footer-lien">
-            <div class="footer-icon">
-                <a href="/accueil"><img src="/icons/home.png" class="footer-icon" alt="accueil"/></a>
-            </div>
-            <div class="footer-icon">
-                <span>Accueil</span>
-            </div>
+            <a href="/accueil"><img src="/icons/home.png" class="footer-icon" alt="accueil"/></a>
+            <span class="footer-text">Accueil</span>
         </div>
-
         <div class="footer-lien">
-            <div>
-                <a href="/celliers"><img src="/icons/moncellier.png" class="footer-icon" alt="cellier"/></a>
-            </div>
-            <div class="footer-icon">
-                <span>Accueil</span>
-            </div>
+            <a href="/celliers"><img src="/icons/moncellier.png" class="footer-icon" alt="cellier"/></a>
+            <span class="footer-text">Cellier</span>
         </div>
-
         <div class="footer-lien">
-            <div>
-                <a href="/recherche"><img src="/icons/rechercher.png" class="footer-icon" alt="recherche"/></a>
-            </div>
-            <div class="footer-icon">
-                <span>Accueil</span>
-            </div>
+            <a href="/recherche"><img src="/icons/rechercher.png" class="footer-icon" alt="recherche"/></a>
+            <span class="footer-text">Recherche</span>
         </div>
-
         <div class="footer-lien">
-            <div>
-                <a href="/user/{{ Auth::user()->id }}"><img src="/icons/profil.png" class="footer-icon" alt="profil"/></a>
-            </div>
-            <div class="footer-icon">
-                <span>Accueil</span>
-            </div>
+            <a href="/user/{{ Auth::user()->id }}"><img src="/icons/profil.png" class="footer-icon" alt="profil"/></a>
+            <span class="footer-text">Profil</span>
         </div>
     </div>
 </footer>

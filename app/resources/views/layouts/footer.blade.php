1d0f1d0410127f11b3f6b0c812f870c6699287c3
MarieLoup46 <melaniebel41@gmail.com>
2023-11-21 12:09:36 -0500

    Menu navigation doit être repensée

--- app/resources/views/layouts/footer.blade.php
+++ app/resources/views/layouts/footer.blade.php
@@ -1,8 +1,16 @@
 <footer class="footer">
-    <a href="/accueil"><img src="/icons/home.png" class="footer-icon" alt="accueil"/></a>
-    <a href="/celliers"><img src="/icons/moncellier.png" class="footer-icon" alt="cellier"/></a>
-    <a href="/recherche"><img src="/icons/rechercher.png" class="footer-icon" alt="recherche"/></a>
-    @if(!auth()->guest())
-        <a href="/user/{{ Auth::user()->id }}"><img src="/icons/profil.png" class="footer-icon" alt="profil"/></a>
-    @endif
+    <div class="footer_nav">
+        <a href="/accueil"><img src="/icons/home.png" class="footer-icon" alt="accueil"/>Accueil</a>
+    </div>
+    <div class="footer_nav">
+        <a href="/celliers"><img src="/icons/moncellier.png" class="footer-icon" alt="cellier"/>Cellier</a>
+    </div>
+    <div class="footer_nav">
+        <a href="/recherche"><img src="/icons/rechercher.png" class="footer-icon" alt="recherche"/>Recherche</a>
+    </div>
+    <div class="footer_nav">
+        @if(!auth()->guest())
+            <a href="/user/{{ Auth::user()->id }}"><img src="/icons/profil.png" class="footer-icon" alt="profil"/>Profil</a>
+        @endif
+    </div>
 </footer>

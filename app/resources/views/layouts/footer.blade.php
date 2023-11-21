c819a02e45e235d5611fee0f87fa9a12be7f05e1
MarieLoup46 <melaniebel41@gmail.com>
2023-11-21 13:17:46 -0500

    Essais/Modifs footer menu

--- app/resources/views/layouts/footer.blade.php
+++ app/resources/views/layouts/footer.blade.php
@@ -1,16 +1,39 @@
 <footer class="footer">
-    <div class="footer_nav">
-        <a href="/accueil"><img src="/icons/home.png" class="footer-icon" alt="accueil"/>Accueil</a>
-    </div>
-    <div class="footer_nav">
-        <a href="/celliers"><img src="/icons/moncellier.png" class="footer-icon" alt="cellier"/>Cellier</a>
-    </div>
-    <div class="footer_nav">
-        <a href="/recherche"><img src="/icons/rechercher.png" class="footer-icon" alt="recherche"/>Recherche</a>
-    </div>
-    <div class="footer_nav">
-        @if(!auth()->guest())
-            <a href="/user/{{ Auth::user()->id }}"><img src="/icons/profil.png" class="footer-icon" alt="profil"/>Profil</a>
-        @endif
+    <div class="footer-nav">
+        <div class="footer-lien">
+            <div class="footer-icon">
+                <a href="/accueil"><img src="/icons/home.png" class="footer-icon" alt="accueil"/></a>
+            </div>
+            <div class="footer-icon">
+                <span>Accueil</span>
+            </div>
+        </div>
+
+        <div class="footer-lien">
+            <div>
+                <a href="/celliers"><img src="/icons/moncellier.png" class="footer-icon" alt="cellier"/></a>
+            </div>
+            <div class="footer-icon">
+                <span>Accueil</span>
+            </div>
+        </div>
+
+        <div class="footer-lien">
+            <div>
+                <a href="/recherche"><img src="/icons/rechercher.png" class="footer-icon" alt="recherche"/></a>
+            </div>
+            <div class="footer-icon">
+                <span>Accueil</span>
+            </div>
+        </div>
+
+        <div class="footer-lien">
+            <div>
+                <a href="/user/{{ Auth::user()->id }}"><img src="/icons/profil.png" class="footer-icon" alt="profil"/></a>
+            </div>
+            <div class="footer-icon">
+                <span>Accueil</span>
+            </div>
+        </div>
     </div>
 </footer>
